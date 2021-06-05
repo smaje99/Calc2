763bdcbdea0356610b6f1cf743639c547edb6706
Sergio Majé <smajefranco@gmail.com>
2021-06-05 13:53:19 -0500

    Refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayDeque;
+
 public class Main extends JFrame {
     private ArrayDeque<String> ecuacion;
     private int len;
     private boolean inicio;
+
     public Main() {
         super("Calc");
         len=0;
@@ -17,9 +19,11 @@ public class Main extends JFrame {
         pack();
         setLocationRelativeTo(null);
     }
-    public static void main(String args[]) {
+
+    public static void main(String[] args) {
         new Main().setVisible(true);
-    }//512 code line
+    }// 382 code line
+
     private void init() {
         JTextField field = new JTextField();
         field.setFont(new Font("Ubuntu Mono Regular",Font.BOLD,20));
@@ -36,226 +40,27 @@ public class Main extends JFrame {
         field.setBackground(Color.WHITE);
         field.setBorder(BorderFactory.createMatteBorder(1,1,0,1,Color.BLACK));
         field1.setBorder(BorderFactory.createMatteBorder(0,1,1,1,Color.BLACK));
-        JButton cero = button("0", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (inicio){
-                    field.setText("0");
-                    inicio=false;
-                }else field.setText(field.getText()+"0");
-            }
-        });
-        JButton uno = button("1", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (inicio){
-                    field.setText("1");
-                    inicio=false;
-                }else field.setText(field.getText()+"1");
-            }
-        });
-        JButton dos = button("2", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (inicio){
-                    field.setText("2");
-                    inicio=false;
-                }else field.setText(field.getText()+"2");
-            }
-        });
-        JButton tres = button("3", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (inicio){
-                    field.setText("3");
-                    inicio=false;
-                }else field.setText(field.getText()+"3");
-            }
-        });
-        JButton cuatro = button("4", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (inicio){
-                    field.setText("4");
-                    inicio=false;
-                }else field.setText(field.getText()+"4");
-            }
-        });
-        JButton cinco = button("5", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (inicio){
-                    field.setText("5");
-                    inicio=false;
-                }else field.setText(field.getText()+"5");
-            }
-        });
-        JButton seis = button("6", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (inicio){
-                    field.setText("6");
-                    inicio=false;
-                }else field.setText(field.getText()+"6");
-            }
-        });
-        JButton siete = button("7", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (inicio){
-                    field.setText("7");
-                    inicio=false;
-                }else field.setText(field.getText()+"7");
-            }
-        });
-        JButton ocho = button("8", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (inicio){
-                    field.setText("8");
-                    inicio=false;
-                }else field.setText(field.getText()+"8");
-            }
-        });
-        JButton nueve = button("9", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (inicio){
-                    field.setText("9");
-                    inicio=false;
-                }else field.setText(field.getText()+"9");
-            }
-        });
-        JButton division = button("/", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!inicio){
-                    if (ecuacion.isEmpty() || (Character.isDigit(field.getText().charAt(field.getText().length()-1)) || field.getText().endsWith(")"))){
-                        if (!field.getText().endsWith(")")) ecuacion.push(field.getText().substring(len));
-                        ecuacion.push("/");
-                        field.setText(field.getText()+"/");
-                        len=field.getText().length();
-                    }
-                }
-            }
-        });
-        JButton suma = button("+", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!inicio){
-                    if (ecuacion.isEmpty() || (Character.isDigit(field.getText().charAt(field.getText().length()-1)) || field.getText().endsWith(")"))){
-                        if (!field.getText().endsWith(")")) ecuacion.push(field.getText().substring(len));
-                        ecuacion.push("+");
-                        field.setText(field.getText()+"+");
-                        len=field.getText().length();
-                    }
-                }
-            }
-        });
-        JButton resta = button("-", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!inicio){
-                    if (ecuacion.isEmpty() || (Character.isDigit(field.getText().charAt(field.getText().length()-1)) || field.getText().endsWith(")"))){
-                        if (!field.getText().endsWith(")")) ecuacion.push(field.getText().substring(len));
-                        ecuacion.push("-");
-                        field.setText(field.getText()+"-");
-                        len=field.getText().length();
-                    }
-                }
-            }
-        });
-        JButton multiplicacion = button("*", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!inicio){
-                    if (ecuacion.isEmpty() || (Character.isDigit(field.getText().charAt(field.getText().length()-1)) || field.getText().endsWith(")"))){
-                        if (!field.getText().endsWith(")")) ecuacion.push(field.getText().substring(len));
-                        ecuacion.push("*");
-                        field.setText(field.getText()+"*");
-                        len=field.getText().length();
-                    }
-                }
-            }
-        });
-        JButton punto = button(".", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (inicio){
-                    inicio=false;
-                    field.setText("0.");
-                }else if (!Character.isDigit(field.getText().charAt(field.getText().length()-1))) field.setText(field.getText()+"0.");
-                else field.setText(field.getText()+".");
-            }
-        });
-        JButton potencia = button("<html>x<SUP>y</SUP></html>", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!inicio){
-                    if (ecuacion.isEmpty() || (Character.isDigit(field.getText().charAt(field.getText().length()-1)) || field.getText().endsWith(")"))){
-                        if (!field.getText().endsWith(")")) ecuacion.push(field.getText().substring(len));
-                        ecuacion.push("^");
-                        field.setText(field.getText()+"^");
-                        len=field.getText().length();
-                    }
-                }
-            }
-        });
-        JButton parenA = button("(", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (inicio){
-                    field.setText("(");
-                    ecuacion.push("(");
-                    inicio=false;
-                    len=field.getText().length();
-                }else if (!Character.isDigit(ecuacion.getFirst().charAt(0))) {
-                    field.setText(field.getText()+"(");
-                    ecuacion.push("(");
-                    len=field.getText().length();
-                }
-            }
-        });
-        JButton parenC = button(")", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (!inicio && (Character.isDigit(field.getText().charAt(field.getText().length()-1)) || field.getText().endsWith(")"))){
-                    if (!field.getText().endsWith(")")) ecuacion.push(field.getText().substring(len));
-                    ecuacion.push(")");
-                    field.setText(field.getText()+")");
-                    len=field.getText().length();
-                }
-            }
-        });
-        JButton igual = button("=", Color.BLACK, new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try{
-                    if (!inicio){
-                        if (field.getText().endsWith(")")) {
-                            field1.setText(new Operaciones(ecuacion).operar());
-                            ecuacion=new ArrayDeque<>();
-                            inicio=true;
-                            len=0;
-                        } else if (Character.isDigit(field.getText().charAt(field.getText().length()-1))) {
-                            ecuacion.push(field.getText().substring(len));
-                            field1.setText(new Operaciones(ecuacion).operar());
-                            ecuacion=new ArrayDeque<>();
-                            inicio=true;
-                            len=0;
-                        }
-                    }
-                }catch (IllegalArgumentException e1){
-                    field1.setForeground(Color.RED);
-                    field1.setText(e1.getMessage());
-                    field1.setForeground(Color.BLACK);
-                    field.setText("");
-                    ecuacion=new ArrayDeque<>();
-                    inicio=true;
-                    len=0;
-                }
-            }
-        });
+
+        JButton cero = button("0", Color.BLACK, e -> concatNumber(field, '0'));
+        JButton uno = button("1", Color.BLACK, e -> concatNumber(field, '1'));
+        JButton dos = button("2", Color.BLACK, e -> concatNumber(field, '2'));
+        JButton tres = button("3", Color.BLACK, e -> concatNumber(field, '3'));
+        JButton cuatro = button("4", Color.BLACK, e -> concatNumber(field, '4'));
+        JButton cinco = button("5", Color.BLACK, e -> concatNumber(field, '5'));
+        JButton seis = button("6", Color.BLACK, e -> concatNumber(field, '6'));
+        JButton siete = button("7", Color.BLACK, e -> concatNumber(field, '7'));
+        JButton ocho = button("8", Color.BLACK, e -> concatNumber(field, '8'));
+        JButton nueve = button("9", Color.BLACK, e -> concatNumber(field, '9'));
+        JButton division = button("/", Color.BLACK, e -> concatOperator(field, '/'));
+        JButton suma = button("+", Color.BLACK, e -> concatOperator(field, '+'));
+        JButton resta = button("-", Color.BLACK, e -> concatOperator(field, '-'));
+        JButton multiplicacion = button("*", Color.BLACK, e -> concatOperator(field, '*'));
+        JButton punto = button(".", Color.BLACK, e -> dot(field));
+        JButton potencia = button("<html>x<SUP>y</SUP></html>", Color.BLACK, e -> concatOperator(field, '^'));
+        JButton parenA = button("(", Color.BLACK, e -> parentA(field));
+        JButton parenC = button(")", Color.BLACK, e -> concatOperator(field, ')'));
+        JButton igual = button("=", Color.BLACK, e -> operate(field, field1));
+
         Constrains.addCompIy(field,this,0,0,8,1,1,1,25,10,0,10,20,GridBagConstraints.SOUTH,GridBagConstraints.BOTH);
         Constrains.addCompIy(field1,this, 0, 1, 8, 1, 1, 1, 0, 10, 5, 10,20, GridBagConstraints.NORTH, GridBagConstraints.BOTH);
         Constrains.addComp(siete,this,3,5,1,1,1,1,3,10,3,3,GridBagConstraints.CENTER,GridBagConstraints.BOTH);
@@ -278,11 +83,84 @@ public class Main extends JFrame {
         Constrains.addComp(suma,this,6,8,1,1,1,1,3,3,15,3,GridBagConstraints.CENTER,GridBagConstraints.BOTH);
         Constrains.addComp(parenC,this,7,7,1,1,1,1,3,3,3,10,GridBagConstraints.CENTER,GridBagConstraints.BOTH);
     }
-    private JButton button(String name,Color color,AbstractAction action){
-        JButton button=new JButton(name);
+
+    private void concatNumber(JTextField field, char character) {
+        String s = String.valueOf(character);
+        if (inicio){
+            field.setText(s);
+            inicio = false;
+        }else field.setText(field.getText() + s);
+    }
+
+    private void concatOperator(JTextField field, char operator) {
+        String s = String.valueOf(operator);
+        if (!inicio && (ecuacion.isEmpty()
+                || (Character.isDigit(field.getText().charAt(field.getText().length() - 1))
+                || field.getText().endsWith(")")))) {
+            if (!field.getText().endsWith(")")) ecuacion.push(field.getText().substring(len));
+            ecuacion.push(s);
+            field.setText(field.getText() + s);
+            len = field.getText().length();
+        }
+    }
+
+    private void operate(JTextField field, JTextField field1) {
+        try{
+            if (!inicio){
+                if (field.getText().endsWith(")")) operateEquation(field1);
+                else if (Character.isDigit(field.getText().charAt(field.getText().length() - 1))) {
+                    ecuacion.push(field.getText().substring(len));
+                    operateEquation(field1);
+                }
+            }
+        }catch (IllegalArgumentException e){
+            field1.setForeground(Color.RED);
+            field1.setText(e.getMessage());
+            field1.setForeground(Color.BLACK);
+            field.setText("");
+            resetEquation();
+        }
+    }
+
+    private void operateEquation(JTextField field1) {
+        field1.setText(new Operaciones(ecuacion).operar());
+        resetEquation();
+    }
+
+    private void resetEquation() {
+        ecuacion = new ArrayDeque<>();
+        inicio = true;
+        len = 0;
+    }
+
+    private void parentA(JTextField field) {
+        String parenA = "(";
+        if (inicio){
+            field.setText(parenA);
+            ecuacion.push(parenA);
+            inicio = false;
+            len = field.getText().length();
+        }else if (!Character.isDigit(ecuacion.getFirst().charAt(0))) {
+            field.setText(field.getText() + parenA);
+            ecuacion.push(parenA);
+            len=field.getText().length();
+        }
+    }
+
+    private void dot(JTextField field) {
+        if (inicio){
+            inicio = false;
+            field.setText("0.");
+        } else if (!Character.isDigit(field.getText().charAt(field.getText().length() - 1))) {
+            field.setText(field.getText() + "0.");
+        } else field.setText(field.getText() + ".");
+    }
+
+    private JButton button(String name, Color color, ActionListener action){
+        JButton button = new JButton(name);
         button.setForeground(color);
-        button.setFont(new Font("Ubuntu Mono Regular",Font.BOLD,13));
-        button.setBackground((Character.isDigit(name.charAt(0)) && name.length()==1)?new Color(238, 238, 238):new Color(205,205,205));
+        button.setFont(new Font("Ubuntu Mono Regular", Font.BOLD, 13));
+        button.setBackground((Character.isDigit(name.charAt(0)) && name.length() == 1) ? new Color(238, 238, 238) : new Color(205, 205, 205));
         button.setBorder(new RoundedBorder(10));
         button.addActionListener(action);
         return button;
--- src/Operaciones.java
+++ src/Operaciones.java
@@ -1,14 +1,17 @@
 import java.util.ArrayDeque;
+
 public class Operaciones {
     private ArrayDeque<String> ecuacion;
+
     public Operaciones(ArrayDeque<String> ecuacion) {
         this.ecuacion = ecuacion;
     }
+
     private ArrayDeque<String> postfija(){
-        ArrayDeque<String> postfija=new ArrayDeque<>();
-        ArrayDeque<String> pila=new ArrayDeque<>();
+        ArrayDeque<String> postfija = new ArrayDeque<>();
+        ArrayDeque<String> pila = new ArrayDeque<>();
         while (!ecuacion.isEmpty()){
-            String n=ecuacion.pollLast();
+            String n = ecuacion.pollLast();
             if (Character.isDigit(n.charAt(0))) postfija.push(n);
             else if (pila.isEmpty()) pila.push(n);
             else if (n.equals(")")){
@@ -16,7 +19,7 @@ public class Operaciones {
                 while (!pila.isEmpty() && !(n1=pila.pop()).equals("(")) {
                     postfija.push(n1);
                 }
-            }else if (!(prioridadExpresion(n.charAt(0))>prioridadPila(pila.getFirst().charAt(0)))){
+            }else if (!(prioridadExpresion(n.charAt(0)) > prioridadPila(pila.getFirst().charAt(0)))){
                 postfija.push(pila.pop());
                 pila.push(n);
             }else pila.push(n);
@@ -24,38 +27,42 @@ public class Operaciones {
         while (!pila.isEmpty()) postfija.push(pila.pop());
         return postfija;
     }
+
     private int prioridadExpresion(char n){
-        if (n=='^') return 4;
-        else if (n=='*' || n=='/') return 2;
-        else if (n=='+' || n=='-') return 1;
+        if (n == '^') return 4;
+        else if (n == '*' || n == '/') return 2;
+        else if (n == '+' || n == '-') return 1;
         else return 5;
     }
+
     private int prioridadPila(char n){
-        if (n=='^') return 3;
-        else if (n=='*' || n=='/') return 2;
-        else if (n=='+' || n=='-') return 1;
+        if (n == '^') return 3;
+        else if (n == '*' || n == '/') return 2;
+        else if (n == '+' || n == '-') return 1;
         else return 0;
     }
+
     public String operar() throws IllegalArgumentException{
-        ArrayDeque<String> posfija=postfija();
-        ArrayDeque<String> pila=new ArrayDeque<>();
+        ArrayDeque<String> posfija = postfija();
+        ArrayDeque<String> pila = new ArrayDeque<>();
         if (postfija().contains("(")) throw new IllegalArgumentException("SYNTAX ERROR");
         while (!posfija.isEmpty()){
-            String n=posfija.pollLast();
+            String n = posfija.pollLast();
             if (Character.isDigit(n.charAt(0))) pila.push(n);
             else {
-                double o2= Double.parseDouble(pila.pop());
-                double o1= Double.parseDouble(pila.pop());
-                pila.push(String.valueOf(operar(n.charAt(0),o1,o2)));
+                double o2 = Double.parseDouble(pila.pop());
+                double o1 = Double.parseDouble(pila.pop());
+                pila.push(String.valueOf(operar(n.charAt(0), o1, o2)));
             }
         }
         return pila.pop();
     }
-    private double operar(char n,double o1,double o2){
-        if (n=='+') return o1+o2;
-        else if (n=='-') return o1-o2;
-        else if (n=='*') return o1*o2;
-        else if (n=='/') return o1/o2;
-        else return Math.pow(o1,o2);
+
+    private double operar(char n, double o1, double o2){
+        if (n == '+') return o1 + o2;
+        else if (n == '-') return o1 - o2;
+        else if (n == '*') return o1 * o2;
+        else if (n == '/') return o1 / o2;
+        else return Math.pow(o1, o2);
     }
 }
